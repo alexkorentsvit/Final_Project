b3885e88ad040380021ece7e44666d388c21725e
alexkorentsvit <alex.dota@gmail.com>
2017-06-28 21:20:44 +0300

    final commit

--- logic.py
+++ logic.py
@@ -1,3 +1,4 @@
+import time
 def calc_matrix(spectrum_vhid,data):
     spectrum = spectrum_vhid.split(',')
     him_el = []
@@ -95,5 +96,8 @@ def calc_matrix(spectrum_vhid,data):
     H = round(matrix[1][5] - matrix[1][2]*N - matrix[1][3]*C - matrix[1][4]*Na, 3)
     O = round(matrix[0][5] - matrix[0][1]*H - matrix[0][2]*N - matrix[0][3]*C - matrix[0][4]*Na, 3)
     return [{'el':{'name':him_el[0][0], 'spectrum': him_el[0][1]},'body':O}, {'el':{'name':him_el[1][0], 'spectrum': him_el[1][1]},'body':H}, {'el':{'name':him_el[2][0], 'spectrum': him_el[2][1]},'body':N}, {'el':{'name':him_el[3][0], 'spectrum': him_el[3][1]},'body':C}, {'el':{'name':him_el[4][0], 'spectrum': him_el[4][1]},'body':Na}]
-
+#begin_time = time.time()
+#
 #print(calc_matrix('4.26,4.48,4.4,2.58,3.14', [{'name':'Oxygen','spectrum':'4.0,5.0,5.0,3.1,3.5' },{'name':'Hidrogen','spectrum': '2.0,1.0,2.0,2.5,1.5'},{'name':'Nitrogen','spectrum':'1.0,1.0,1.0,1.0,1.0' },{'name':'Carbon','spectrum': '3.1,3.7,3.5,1.8,2.6'},{'name':'Sodium','spectrum':'5.2,4.5,3.8,4.5,4.9'}]))
+#end_time = time.time()
+#print(str(end_time - begin_time))
